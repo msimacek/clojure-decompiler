028bb10b727b2ca772c5b530a3cc484efd897102
Michael Simacek <msimacek@redhat.com>
2015-03-11 23:39:17 +0100

    Set project URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject decompiler "0.1.0-SNAPSHOT"
   :description "Clojure decompiler"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/msimacek/clojure-decompiler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main decompiler.core
