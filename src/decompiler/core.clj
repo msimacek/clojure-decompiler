eb13dd40f5eb58ef09f718f3850d28881e73defb
Michael Simacek <msimacek@redhat.com>
2015-03-15 16:21:15 +0100

    Reformat code

--- src/decompiler/core.clj
+++ src/decompiler/core.clj
@@ -41,124 +41,141 @@
            result []]
       (if insn
         (condp instance? insn
-          IFNULL (let [target (+ index (.getIndex insn))
-                       [[_ get-false-insn] & code] code
-                       _ (assert (instance? GETSTATIC get-false-insn))
-                       _ (assert (= (insn-field get-false-insn pool) "java.lang.Boolean/FALSE"))
-                       [[_ acmpeq-insn] & code] code
-                       _ (assert (instance? IF_ACMPEQ acmpeq-insn))
-                       condition (peek stack)
-                       stack (pop-n stack 2) ; 1 -1 + 2
-                       index-op #(fn [[i _]] (% i %2))
-                       [false-branch _] (code->expr clazz method fields
-                                                    (take-while (index-op < target) code) stack)
-                       false-branch (peek false-branch)
-                       end (:target false-branch)
-                       false-branch (:stack-top false-branch false-branch)
-                       [true-branch _] (if end
-                                         (code->expr clazz method fields
-                                                     (take-while (index-op < end)
-                                                                 (drop-while (index-op < target) code))
-                                                     stack))
-                       expr {:type :if
-                             :cond condition
-                             :false-branch (peek true-branch)
-                             :true-branch false-branch}]
-                   (recur (drop-while (index-op < (or end target)) code)
-                          (conj stack expr)
-                          vars fields
-                          (conj result expr)))
-          GETSTATIC (recur code
-                           (conj stack (cond
-                                         (= (.getClassName insn pool) class-name)
-                                         (fields (.getFieldName insn pool))
-                                         (= (insn-field insn pool) "java.lang.Boolean/TRUE")
-                                         {:type :const :value true}
-                                         (= (insn-field insn pool) "java.lang.Boolean/FALSE")
-                                         {:type :const :value false}
-                                         :default {:type :get-field
-                                                   :class (.getClassName insn pool)
-                                                   :field (.getFieldName insn pool)}))
-                           vars fields result)
-          PUTSTATIC (if (= (.getClassName insn pool) class-name)
-                      (recur code ; TODO putstatic on other objects
-                             (pop stack)
-                             vars
-                             (assoc fields (.getFieldName insn pool) (peek stack))
-                             result))
-          LoadInstruction (recur code
-                                 (conj stack (nth vars (.getIndex insn)))
-                                 vars fields result)
-          StoreInstruction (let [index (.getIndex insn)
-                                 vars (if-let [local (nth vars index nil)]
-                                        (if (#{:local :arg} (:type local))
-                                          (assoc vars index (assoc local :assign (peek stack))))
-                                        (conj vars {:type :local
-                                                    :initial (peek stack)
-                                                    :index index}))]
-                             (recur code (pop stack) vars fields result))
-          GotoInstruction (let [expr (if (== (.getIndex insn) (- index))
-                                       {:type :recur
-                                        :args (subvec vars 1 arg-count)}
-                                       {:type :goto
-                                        :target (+ index (.getIndex insn))
-                                        :stack-top (peek stack)})]
-                            [(conj result expr) fields])
-          DUP (recur code (conj stack (peek stack)) vars fields result)
-          LDC (recur code
-                     (conj stack {:type :const :value (.getValue insn pool)})
-                     vars fields result)
-          LDC_W (recur code ;FIXME copypaste
-                       (conj stack {:type :const :value (.getValue insn pool)})
-                       vars fields result)
-          LDC2_W (recur code ;FIXME copypaste
-                        (conj stack {:type :const :value (.getValue insn pool)})
-                        vars fields result)
-          ACONST_NULL (recur code (conj stack {:type :const :value nil}) vars fields result)
-          ConstantPushInstruction (recur code
-                                         (conj stack {:type :const
-                                                      :value (.getValue insn)})
-                                         vars fields result)
-          INVOKESTATIC (let [argc (count (.getArgumentTypes insn pool))
-                             expr (condp = (insn-method insn pool)
-                                    "clojure.lang.RT/var"
-                                    {:type :var
-                                     :ns (demunge (:value (peek-at stack 1)))
-                                     :name (demunge (:value (peek stack)))}
-                                    "java.lang.Long/valueOf"
-                                    (peek stack)
-                                    "java.lang.Double/valueOf"
-                                    (peek stack)
-                                    "java.lang.Integer/valueOf"
-                                    ;may appear when boxing int return from interop
-                                    (peek stack)
-                                    "clojure.lang.RT/readString"
-                                    {:type :const
-                                     :value (clojure.lang.RT/readString (:value (peek stack)))}
-                                    {:type :invoke-static
-                                     :class (.getClassName insn pool)
-                                     :method (.getMethodName insn pool)
-                                     :args (peek-n stack argc)})]
-                         (recur code
-                                (conj (pop-n stack argc) expr)
-                                vars fields
-                                (conj result expr)))
-          INVOKEVIRTUAL (if (= (.getMethodName insn pool) "getRawRoot") ; TODO and is var
-                          (recur code stack vars fields result) ; we have the var already
-                          (recur code stack vars fields result)) ; TODO handle this
-          INVOKEINTERFACE (if (= (.getMethodName insn pool) "invoke") ;TODO check type
-                            (let [argc (count (.getArgumentTypes insn pool))
-                                  argc1 (inc argc)
-                                  expr {:type :invoke
-                                        :args (peek-n stack argc)
-                                        :name (:name (peek-at stack argc))}]
-                              (recur code
-                                     (conj (pop-n stack argc1) expr)
-                                     vars fields
-                                     (conj result expr)))
-                            (recur code (pop stack) vars fields result)) ; TODO handle interop
-          ARETURN [(conj result (peek stack)) fields]
-          RETURN [result fields]
+          IFNULL
+          (let [target (+ index (.getIndex insn))
+                [[_ get-false-insn] & code] code
+                _ (assert (instance? GETSTATIC get-false-insn))
+                _ (assert (= (insn-field get-false-insn pool) "java.lang.Boolean/FALSE"))
+                [[_ acmpeq-insn] & code] code
+                _ (assert (instance? IF_ACMPEQ acmpeq-insn))
+                condition (peek stack)
+                stack (pop-n stack 2) ; 1 -1 + 2
+                index-op #(fn [[i _]] (% i %2))
+                [false-branch _] (code->expr clazz method fields
+                                             (take-while (index-op < target) code) stack)
+                false-branch (peek false-branch)
+                end (:target false-branch)
+                false-branch (:stack-top false-branch false-branch)
+                [true-branch _] (if end
+                                  (code->expr clazz method fields
+                                              (take-while (index-op < end)
+                                                          (drop-while (index-op < target) code))
+                                              stack))
+                expr {:type :if
+                      :cond condition
+                      :false-branch (peek true-branch)
+                      :true-branch false-branch}]
+            (recur (drop-while (index-op < (or end target)) code)
+                   (conj stack expr)
+                   vars fields
+                   (conj result expr)))
+          GETSTATIC
+          (recur code
+                 (conj stack (cond
+                               (= (.getClassName insn pool) class-name)
+                               (fields (.getFieldName insn pool))
+                               (= (insn-field insn pool) "java.lang.Boolean/TRUE")
+                               {:type :const :value true}
+                               (= (insn-field insn pool) "java.lang.Boolean/FALSE")
+                               {:type :const :value false}
+                               :default {:type :get-field
+                                         :class (.getClassName insn pool)
+                                         :field (.getFieldName insn pool)}))
+                 vars fields result)
+          PUTSTATIC
+          (if (= (.getClassName insn pool) class-name)
+            (recur code ; TODO putstatic on other objects
+                   (pop stack)
+                   vars
+                   (assoc fields (.getFieldName insn pool) (peek stack))
+                   result))
+          LoadInstruction
+          (recur code
+                 (conj stack (nth vars (.getIndex insn)))
+                 vars fields result)
+          StoreInstruction
+          (let [index (.getIndex insn)
+                vars (if-let [local (nth vars index nil)]
+                       (if (#{:local :arg} (:type local))
+                         (assoc vars index (assoc local :assign (peek stack))))
+                       (conj vars {:type :local
+                                   :initial (peek stack)
+                                   :index index}))]
+            (recur code (pop stack) vars fields result))
+          GotoInstruction
+          (let [expr (if (== (.getIndex insn) (- index))
+                       {:type :recur
+                        :args (subvec vars 1 arg-count)}
+                       {:type :goto
+                        :target (+ index (.getIndex insn))
+                        :stack-top (peek stack)})]
+            [(conj result expr) fields])
+          DUP
+          (recur code (conj stack (peek stack)) vars fields result)
+          LDC
+          (recur code
+                 (conj stack {:type :const :value (.getValue insn pool)})
+                 vars fields result)
+          LDC_W
+          (recur code ;FIXME copypaste
+                 (conj stack {:type :const :value (.getValue insn pool)})
+                 vars fields result)
+          LDC2_W
+          (recur code ;FIXME copypaste
+                 (conj stack {:type :const :value (.getValue insn pool)})
+                 vars fields result)
+          ACONST_NULL
+          (recur code (conj stack {:type :const :value nil}) vars fields result)
+          ConstantPushInstruction
+          (recur code
+                 (conj stack {:type :const
+                              :value (.getValue insn)})
+                 vars fields result)
+          INVOKESTATIC
+          (let [argc (count (.getArgumentTypes insn pool))
+                expr (condp = (insn-method insn pool)
+                       "clojure.lang.RT/var"
+                       {:type :var
+                        :ns (demunge (:value (peek-at stack 1)))
+                        :name (demunge (:value (peek stack)))}
+                       "java.lang.Long/valueOf"
+                       (peek stack)
+                       "java.lang.Double/valueOf"
+                       (peek stack)
+                       "java.lang.Integer/valueOf"
+                       ;may appear when boxing int return from interop
+                       (peek stack)
+                       "clojure.lang.RT/readString"
+                       {:type :const
+                        :value (clojure.lang.RT/readString (:value (peek stack)))}
+                       {:type :invoke-static
+                        :class (.getClassName insn pool)
+                        :method (.getMethodName insn pool)
+                        :args (peek-n stack argc)})]
+            (recur code
+                   (conj (pop-n stack argc) expr)
+                   vars fields
+                   (conj result expr)))
+          INVOKEVIRTUAL
+          (if (= (.getMethodName insn pool) "getRawRoot") ; TODO and is var
+            (recur code stack vars fields result) ; we have the var already
+            (recur code stack vars fields result)) ; TODO handle this
+          INVOKEINTERFACE
+          (if (= (.getMethodName insn pool) "invoke") ;TODO check type
+            (let [argc (count (.getArgumentTypes insn pool))
+                  argc1 (inc argc)
+                  expr {:type :invoke
+                        :args (peek-n stack argc)
+                        :name (:name (peek-at stack argc))}]
+              (recur code
+                     (conj (pop-n stack argc1) expr)
+                     vars fields
+                     (conj result expr)))
+            (recur code (pop stack) vars fields result)) ; TODO handle interop
+          ARETURN
+          [(conj result (peek stack)) fields]
+          RETURN
+          [result fields]
           (recur code stack vars fields result))
         [(if (seq stack) (conj result (peek stack)) result) fields]))))
 
--- test/decompiler/core_test.clj
+++ test/decompiler/core_test.clj
@@ -27,28 +27,51 @@
          expected-code (to-code expected-code)]
      `(deftest ~test-name (is (= '~expected-code (compile-and-decompile ~(name test-name) '~code)))))))
 
-
-(deftest-decompile return-0 (defn test-fn [] 0))
-(deftest-decompile return-string (defn test-fn [] "Hello"))
-(deftest-decompile return-long (defn test-fn [] 123456789))
-(deftest-decompile return-big-decimal (defn test-fn [] 12345678901234567890.1M))
-(deftest-decompile return-big-int (defn test-fn [] 12345678901234567890N))
-(deftest-decompile return-double (defn test-fn [] 1.1))
-(deftest-decompile return-nil (defn test-fn [] nil))
-(deftest-decompile return-true (defn test-fn [] true))
-(deftest-decompile return-false (defn test-fn [] false))
-(deftest-decompile simple-clj-call (defn test-fn [] (println "Hello")))
-(deftest-decompile clj-call-param (defn test-fn [arg1] (str arg1)))
-(deftest-decompile clj-call-param2 (defn test-fn [arg1 arg2] (str arg1 arg2)))
-(deftest-decompile clj-call-nested (defn test-fn [arg1 arg2] (str (str arg1 2) 1)))
-(deftest-decompile clj-call-static (defn test-fn [arg1 arg2] (java.lang.Double/compare 1.0 1.00001)))
-(deftest-decompile clj-call-static (defn test-fn [] java.lang.Double/NaN))
-(deftest-decompile unconditional-recur-arg (defn test-fn [arg1] (recur arg1)))
-(deftest-decompile unconditional-recur-expr (defn test-fn [arg1] (recur (str arg1))))
-(deftest-decompile unconditional-recur-const (defn test-fn [arg1] (recur false)))
-(deftest-decompile simple-if (defn test-fn [arg1] (if arg1 1 2)))
-(deftest-decompile if-expr (defn test-fn [arg1 arg2] (if arg1 (str arg1) (str arg2))))
-(deftest-decompile if-expr-child (defn test-fn [arg1 arg2] (str (if arg1 (str arg1) (str arg2)))))
-(deftest-decompile if-expr-cond (defn test-fn [arg1 arg2] (if (str arg1) (str arg1) 0)))
-(deftest-decompile if-no-else (defn test-fn [arg1 arg2] (if arg1 (str arg1))))
-(deftest-decompile if-else-false (defn test-fn [arg1 arg2] (if arg1 (str arg1) false)))
+(deftest-decompile return-0
+  (defn test-fn [] 0))
+(deftest-decompile return-string
+  (defn test-fn [] "Hello"))
+(deftest-decompile return-long
+  (defn test-fn [] 123456789))
+(deftest-decompile return-big-decimal
+  (defn test-fn [] 12345678901234567890.1M))
+(deftest-decompile return-big-int
+  (defn test-fn [] 12345678901234567890N))
+(deftest-decompile return-double
+  (defn test-fn [] 1.1))
+(deftest-decompile return-nil
+  (defn test-fn [] nil))
+(deftest-decompile return-true
+  (defn test-fn [] true))
+(deftest-decompile return-false
+  (defn test-fn [] false))
+(deftest-decompile simple-clj-call
+  (defn test-fn [] (println "Hello")))
+(deftest-decompile clj-call-param
+  (defn test-fn [arg1] (str arg1)))
+(deftest-decompile clj-call-param2
+  (defn test-fn [arg1 arg2] (str arg1 arg2)))
+(deftest-decompile clj-call-nested
+  (defn test-fn [arg1 arg2] (str (str arg1 2) 1)))
+(deftest-decompile clj-call-static
+  (defn test-fn [arg1 arg2] (java.lang.Double/compare 1.0 1.00001)))
+(deftest-decompile clj-call-static
+  (defn test-fn [] java.lang.Double/NaN))
+(deftest-decompile unconditional-recur-arg
+  (defn test-fn [arg1] (recur arg1)))
+(deftest-decompile unconditional-recur-expr
+  (defn test-fn [arg1] (recur (str arg1))))
+(deftest-decompile unconditional-recur-const
+  (defn test-fn [arg1] (recur false)))
+(deftest-decompile simple-if
+  (defn test-fn [arg1] (if arg1 1 2)))
+(deftest-decompile if-expr
+  (defn test-fn [arg1 arg2] (if arg1 (str arg1) (str arg2))))
+(deftest-decompile if-expr-child
+  (defn test-fn [arg1 arg2] (str (if arg1 (str arg1) (str arg2)))))
+(deftest-decompile if-expr-cond
+  (defn test-fn [arg1 arg2] (if (str arg1) (str arg1) 0)))
+(deftest-decompile if-no-else
+  (defn test-fn [arg1 arg2] (if arg1 (str arg1))))
+(deftest-decompile if-else-false
+  (defn test-fn [arg1 arg2] (if arg1 (str arg1) false)))
