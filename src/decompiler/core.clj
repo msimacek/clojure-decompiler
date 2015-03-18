8f253e5bca3dda317a555a6f2ba2ecd24b9bf159
Michael Simacek <msimacek@redhat.com>
2015-03-18 21:01:18 +0100

    Big refactoring

--- src/decompiler/core.clj
+++ src/decompiler/core.clj
@@ -19,7 +19,7 @@
 (defn find-method [clazz method]
   (first (filter #(= (.getName %) method) (.getMethods clazz))))
 
-(defn get-instructions [method]
+(defn get-insns [method]
   (-> method .getCode .getCode InstructionList. .getInstructions))
 
 (defn demunge [what]
@@ -31,161 +31,183 @@
 (defn insn-field [insn pool]
   (str (.getClassName insn pool) \/ (.getFieldName insn pool)))
 
-(defn code->expr [clazz method fields indexed-code stack]
+(defmulti process-insn
+  "Processes one or more instructions, producing altered decompilation context,
+  appending the decompile expression tree representation into :result key of
+  the context.
+  Methods take instruction, instruction index and context map, returning
+  altered context map."
+  (fn [_ insn & _] (class insn)))
+
+(defn process-insns [context]
+  (if (seq (:code context))
+    (let [[[index insn] & code] (:code context)]
+      (recur (process-insn index insn (assoc context :code code))))
+    context))
+
+(defmethod process-insn :default [_ _ context] context)
+
+(defmethod process-insn IFNULL
+  [index insn {:keys [code stack result pool] :as context}]
+  (let [target (+ index (.getIndex insn))
+        [[_ get-false-insn] & code] code
+        _ (assert (instance? GETSTATIC get-false-insn))
+        _ (assert (= (insn-field get-false-insn pool) "java.lang.Boolean/FALSE"))
+        [[_ acmpeq-insn] & code] code
+        _ (assert (instance? IF_ACMPEQ acmpeq-insn))
+        condition (peek stack)
+        stack (pop-n stack 2) ; 1 -1 + 2
+        index< #(fn [[i _]] (< i %))
+        then (process-insns (assoc context
+                                   :code (take-while (index< target) code)))
+        end (-> then :result peek :target)
+        rest-code (drop-while (index< target) code)
+        else (process-insns (assoc context
+                                   :code (if end
+                                           (take-while (index< end) rest-code)
+                                           rest-code)))
+        expr {:type :if
+              :cond condition
+              :else (peek (:stack else))
+              :then (peek (:stack then))}]
+    (assoc context
+           :code (if end (drop-while (index< end) code) ())
+           :stack (conj stack expr)
+           :result (conj result expr))))
+
+(defmethod process-insn LoadInstruction
+  [_ insn {:keys [stack vars] :as context}]
+  (assoc context
+         :stack (conj stack (nth vars (.getIndex insn)))))
+
+(defmethod process-insn GETSTATIC
+  [_ insn {:keys [clazz stack pool fields] :as context}]
   (let [class-name (.getClassName clazz)
-        arg-count (inc (count (.getArgumentTypes method)))
-        pool (ConstantPoolGen. (.getConstantPool clazz))]
-    (loop [[[index insn] & code] indexed-code
-           stack stack
-           vars (mapv (fn [n] {:type :arg :index n}) (range arg-count))
-           fields fields
-           result []]
-      (if insn
-        (condp instance? insn
-          IFNULL
-          (let [target (+ index (.getIndex insn))
-                [[_ get-false-insn] & code] code
-                _ (assert (instance? GETSTATIC get-false-insn))
-                _ (assert (= (insn-field get-false-insn pool) "java.lang.Boolean/FALSE"))
-                [[_ acmpeq-insn] & code] code
-                _ (assert (instance? IF_ACMPEQ acmpeq-insn))
-                condition (peek stack)
-                stack (pop-n stack 2) ; 1 -1 + 2
-                index-op #(fn [[i _]] (% i %2))
-                [then _] (code->expr clazz method fields
-                                     (take-while (index-op < target) code) stack)
-                then (peek then)
-                end (:target then)
-                then (:stack-top then then)
-                rest-code (drop-while (index-op < target) code)
-                [else _] (code->expr clazz method fields
-                                     (if end
-                                       (take-while (index-op < end)
-                                                   rest-code)
-                                       rest-code)
-                                     stack)
-                expr {:type :if
-                      :cond condition
-                      :else (peek else)
-                      :then then}]
-            (recur (if end (drop-while (index-op < end) code) ())
-                   (conj stack expr)
-                   vars fields
-                   (conj result expr)))
-          GETSTATIC
-          (recur code
-                 (conj stack (cond
-                               (= (.getClassName insn pool) class-name)
-                               (fields (.getFieldName insn pool))
-                               (= (insn-field insn pool) "java.lang.Boolean/TRUE")
-                               {:type :const :value true}
-                               (= (insn-field insn pool) "java.lang.Boolean/FALSE")
-                               {:type :const :value false}
-                               :default {:type :get-field
-                                         :class (.getClassName insn pool)
-                                         :field (.getFieldName insn pool)}))
-                 vars fields result)
-          PUTSTATIC
-          (if (= (.getClassName insn pool) class-name)
-            (recur code ; TODO putstatic on other objects
-                   (pop stack)
-                   vars
-                   (assoc fields (.getFieldName insn pool) (peek stack))
-                   result))
-          LoadInstruction
-          (recur code
-                 (conj stack (nth vars (.getIndex insn)))
-                 vars fields result)
-          StoreInstruction
-          (let [index (.getIndex insn)
-                vars (if-let [local (nth vars index nil)]
+        expr (cond
+               (= (.getClassName insn pool) class-name)
+               (fields (.getFieldName insn pool))
+               (= (insn-field insn pool) "java.lang.Boolean/TRUE")
+               {:type :const :value true}
+               (= (insn-field insn pool) "java.lang.Boolean/FALSE")
+               {:type :const :value false}
+               :default {:type :get-field
+                         :class (.getClassName insn pool)
+                         :field (.getFieldName insn pool)})]
+    (assoc context :stack (conj stack expr))))
+
+(defmethod process-insn PUTSTATIC
+  [_ insn {:keys [clazz stack pool fields] :as context}]
+  (if (= (.getClassName insn pool) (.getClassName clazz))
+    (assoc context
+           :stack (pop stack)
+           :fields (assoc fields (.getFieldName insn pool) (peek stack)))))
+
+(defmethod process-insn StoreInstruction
+  [_ insn {:keys [stack vars] :as context}]
+  (let [index (.getIndex insn)]
+    (assoc context
+           :stack (pop stack)
+           :vars (if-let [local (nth vars index nil)]
                        (if (#{:local :arg} (:type local))
                          (assoc vars index (assoc local :assign (peek stack))))
                        (conj vars {:type :local
                                    :initial (peek stack)
-                                   :index index}))]
-            (recur code (pop stack) vars fields result))
-          GotoInstruction
-          (let [expr (if (== (.getIndex insn) (- index))
-                       {:type :recur
-                        :args (subvec vars 1 arg-count)}
-                       {:type :goto
-                        :target (+ index (.getIndex insn))
-                        :stack-top (peek stack)})]
-            [(conj result expr) fields])
-          DUP
-          (recur code (conj stack (peek stack)) vars fields result)
-          LDC
-          (recur code
-                 (conj stack {:type :const :value (.getValue insn pool)})
-                 vars fields result)
-          LDC_W
-          (recur code ;FIXME copypaste
-                 (conj stack {:type :const :value (.getValue insn pool)})
-                 vars fields result)
-          LDC2_W
-          (recur code ;FIXME copypaste
-                 (conj stack {:type :const :value (.getValue insn pool)})
-                 vars fields result)
-          ACONST_NULL
-          (recur code (conj stack {:type :const :value nil}) vars fields result)
-          ConstantPushInstruction
-          (recur code
-                 (conj stack {:type :const
-                              :value (.getValue insn)})
-                 vars fields result)
-          INVOKESTATIC
-          (let [argc (count (.getArgumentTypes insn pool))
-                expr (condp = (insn-method insn pool)
-                       "clojure.lang.RT/var"
-                       {:type :var
-                        :ns (demunge (:value (peek-at stack 1)))
-                        :name (demunge (:value (peek stack)))}
-                       "java.lang.Long/valueOf"
-                       (peek stack)
-                       "java.lang.Double/valueOf"
-                       (peek stack)
-                       "java.lang.Integer/valueOf"
-                       ;may appear when boxing int return from interop
-                       (peek stack)
-                       "clojure.lang.RT/readString"
-                       {:type :const
-                        :value (clojure.lang.RT/readString (:value (peek stack)))}
-                       {:type :invoke-static
-                        :class (.getClassName insn pool)
-                        :method (.getMethodName insn pool)
-                        :args (peek-n stack argc)})]
-            (recur code
-                   (conj (pop-n stack argc) expr)
-                   vars fields
-                   (conj result expr)))
-          INVOKEVIRTUAL
-          (if (= (.getMethodName insn pool) "getRawRoot") ; TODO and is var
-            (recur code stack vars fields result) ; we have the var already
-            (recur code stack vars fields result)) ; TODO handle this
-          INVOKEINTERFACE
-          (if (= (.getMethodName insn pool) "invoke") ;TODO check type
-            (let [argc (count (.getArgumentTypes insn pool))
-                  argc1 (inc argc)
-                  expr {:type :invoke
-                        :args (peek-n stack argc)
-                        :name (:name (peek-at stack argc))}]
-              (recur code
-                     (conj (pop-n stack argc1) expr)
-                     vars fields
-                     (conj result expr)))
-            (recur code (pop stack) vars fields result)) ; TODO handle interop
-          ARETURN
-          [(conj result (peek stack)) fields]
-          RETURN
-          [result fields]
-          (recur code stack vars fields result))
-        [(if (seq stack) (conj result (peek stack)) result) fields]))))
+                                   :index index})))))
 
-(defn method->expr [clazz method fields]
-  (let [insns (get-instructions method)]
-    (code->expr clazz method fields
-                (map vector (reductions + (cons 0 (map #(.getLength %) insns))) insns) [])))
+(defmethod process-insn GotoInstruction
+  [index insn {:keys [stack vars result arg-count] :as context}]
+  (let [expr (if (== (.getIndex insn) (- index))
+               {:type :recur
+                :args (subvec vars 1 arg-count)}
+               {:type :goto
+                :target (+ index (.getIndex insn))})]
+    (assoc context
+           :code nil
+           :result (conj result expr)
+           :stack (if (= (:type expr) :recur)
+                    (conj stack expr)
+                    stack)))) ; recur is expression, goto is auxiliary item
+
+(defmethod process-insn DUP
+  [_ _ {:keys [stack] :as context}]
+  (assoc context :stack (conj stack (peek stack))))
+
+(derive LDC ::ldc-insn)
+(derive LDC_W ::ldc-insn)
+(derive LDC2_W ::ldc-insn)
+
+(defmethod process-insn ::ldc-insn
+  [_ insn {:keys [stack pool] :as context}]
+  (assoc context :stack (conj stack {:type :const :value (.getValue insn pool)})))
+
+(defmethod process-insn ConstantPushInstruction
+  [_ insn {:keys [stack] :as context}]
+  (assoc context :stack (conj stack {:type :const :value (.getValue insn)})))
+
+(defmethod process-insn ACONST_NULL
+  [_ insn {:keys [stack] :as context}]
+  (assoc context :stack (conj stack {:type :const :value nil})))
+
+(defmethod process-insn INVOKESTATIC
+  [_ insn {:keys [stack pool result] :as context}]
+  (let [argc (count (.getArgumentTypes insn pool))
+        expr (condp = (insn-method insn pool)
+               "clojure.lang.RT/var"
+               {:type :var
+                :ns (demunge (:value (peek-at stack 1)))
+                :name (demunge (:value (peek stack)))}
+               "java.lang.Long/valueOf"
+               (peek stack)
+               "java.lang.Double/valueOf"
+               (peek stack)
+               "java.lang.Integer/valueOf"
+               ;may appear when boxing int return from interop
+               (peek stack)
+               "clojure.lang.RT/readString"
+               {:type :const
+                :value (clojure.lang.RT/readString (:value (peek stack)))}
+               {:type :invoke-static
+                :class (.getClassName insn pool)
+                :method (.getMethodName insn pool)
+                :args (peek-n stack argc)})]
+    (assoc context
+           :stack (conj (pop-n stack argc) expr)
+           :result (conj result expr))))
+
+(defmethod process-insn INVOKEINTERFACE
+  [_ insn {:keys [stack pool result] :as context}]
+  ; (if (= (.getMethodName insn pool) "invoke") ;TODO check type
+  (let [argc (count (.getArgumentTypes insn pool))
+        expr {:type :invoke
+              :args (peek-n stack argc)
+              :name (:name (peek-at stack argc))}]
+    (assoc context
+           :stack (conj (pop-n stack (inc argc)) expr)
+           :result (conj result expr))))
+
+(defmethod process-insn ARETURN
+  [_ insn {:keys [stack result] :as context}]
+  (assoc context
+         :code nil
+         :result (conj result (peek stack))))
+
+(defn get-indexed-insns [method]
+  (let [insns (get-insns method)]
+    (map vector (reductions + (cons 0 (map #(.getLength %) insns))) insns)))
+
+(defn method->expr [clazz method & {:as context}]
+  (let [arg-count (inc (count (.getArgumentTypes method)))
+        context (merge {:code (get-indexed-insns method)
+                        :stack []
+                        :clazz clazz
+                        :method method
+                        :fields {}
+                        :result []
+                        :pool (ConstantPoolGen. (.getConstantPool clazz))
+                        :arg-count arg-count
+                        :vars (mapv (fn [n] {:type :arg :index n}) (range arg-count))}
+                       context)]
+    (process-insns context)))
 
 (defn letify [exprs]
   (let [expr (if (vector? exprs) (last exprs) exprs)]
@@ -229,9 +251,9 @@
 (defn decompile-fn [clazz]
   (let [[fn-ns fn-name] (map demunge (string/split (.getClassName clazz) #"\$" 2))
         clinit (find-method clazz "<clinit>")
-        [_ fields] (method->expr clazz clinit {})
+        fields (:fields (method->expr clazz clinit))
         invoke (find-method clazz "invoke") ;TODO multiple arities
-        [exprs _] (method->expr clazz invoke fields)
+        exprs (:result (method->expr clazz invoke :fields fields))
         argc (count (.getArgumentTypes invoke))
         args (mapv #(symbol (str "arg" %)) (range 1 (inc argc)))
         _ (when *debug* (pprint exprs))]
